d1a14f0fb77c247ced366eedd3f87d5a79159ca7
Stephen Whitmore <sww@eight45.net>
2017-11-13 20:56:44 -0800

    pass stdin to omxplayer -- better

--- bin/cmd.js
+++ bin/cmd.js
@@ -5,6 +5,8 @@ var spawn = require('child_process').spawn
 var argv = require('minimist')(process.argv)
 var fs = require('fs')
 var path = require('path')
+var tty = require('tty')
+var request = require('request')
 
 if (argv.serve || argv.s) {
   var server = http.createServer(function (req, res) {
@@ -30,14 +32,27 @@ if (argv.serve || argv.s) {
 } else if (argv._[2]) {
   var ecstatic = require('ecstatic')
 
+  process.stdin.setRawMode(true)
+
   var dir = path.dirname(argv._[2])
   var server = http.createServer(ecstatic({ root: dir }))
   server.listen(5002)
 
   // TODO(noffle): use mdns to find the pi
-  http.get({
-    host: '10.0.0.32',
-    port: 5001,
-    path: '/' + path.basename(argv._[2])
+  var req = request.post({
+    url: 'http://10.0.0.32:5001/' + path.basename(argv._[2]),
+    timeout: 9999999
+  })
+  process.stdin.pipe(req)
+
+  setTimeout(function () {
+    req.write(' ')
+    req.write(' ')
+  }, 1000)
+
+  req.on('response', function () {
+    console.log('done')
+    process.stdin.setRawMode(false)
+    process.exit(0)
   })
 }
